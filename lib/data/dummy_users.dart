b9bc659a83a033ae5529eb4bfd3a9e2c2fa22f84
Fabricio Souza <fabricio.souza.design@gmail.com>
2022-01-21 16:22:19 -0300

    create dummy_users.dart file and class dummyUsers

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,11 @@
+import 'package:crude/models/user.dart';
+
+const dummyUsers = {
+  '1': User(
+    id: '1',
+    name: 'Mari',
+    email: 'maria@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png',
+  ),
+};
