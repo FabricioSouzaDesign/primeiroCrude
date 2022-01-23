b932310a25218eb7be75dba38892594dd020b2b4
Fabricio Souza <fabricio.souza.design@gmail.com>
2022-01-23 20:57:02 -0300

    changes on dummy_users to add 4 fake users

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -3,9 +3,30 @@ import 'package:crude/models/user.dart';
 const dummyUsers = {
   '1': User(
     id: '1',
-    name: 'Mari',
+    name: 'Maria',
     email: 'maria@alunos.com.br',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png',
+  ),
+  '4': User(
+    id: '4',
+    name: 'Naruto',
+    email: 'naruto@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/01/18/08/32/naruto-5927441_960_720.png',
   ),
 };
